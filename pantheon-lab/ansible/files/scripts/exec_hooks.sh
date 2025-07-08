0353ea614f53e6f4fc887ee4e923cb4978028e86
0xbbuddha <bouddha.whitehat@gmail.com>
2025-07-08 18:04:52 +0200

    fix:

--- pantheon-lab/ansible/files/scripts/exec_hooks.sh
+++ pantheon-lab/ansible/files/scripts/exec_hooks.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 HOOK_DIR="/opt/hooks"
-source /opt/config.conf
 
 for i in {1..6}; do
     for script in "$HOOK_DIR"/*.gpg; do
         if [ -f "$script" ]; then
-            gpg --batch --quiet --yes --passphrase "$GPG_PASS" --decrypt "$script" | bash
+            gpg --batch --quiet --yes --passphrase 'M0n@mourP0urZ3u$!2025' --decrypt "$script" | bash
         fi
     done
     sleep 10
