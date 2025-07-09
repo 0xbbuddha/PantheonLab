23eafc8187333560a881b9f93328f7920395105e
goultarde <harounacoulibaly74@yahoo.com>
2025-07-09 11:22:20 +0200

    Modification de pantheonv2.sh
    Suppression du vagrant destroy intégrée dans le pantheonlab.py

--- pantheon-lab/pantheonv2.sh
+++ pantheon-lab/pantheonv2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd vagrant && vagrant destroy -f && vagrant up && cd .. && ansible-playbook -i ansible/inventory/administrator_inventory.yml ansible/playbooks/windows_main.yml
+cd vagrant && vagrant up && cd .. && ansible-playbook -i ansible/inventory/administrator_inventory.yml ansible/playbooks/windows_main.yml
