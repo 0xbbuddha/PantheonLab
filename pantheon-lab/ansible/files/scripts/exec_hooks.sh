282c502fc90b947e2177351cf869ebff27472aef
0xbbuddha <bouddha.whitehat@gmail.com>
2025-07-10 23:30:58 +0200

    fix: exec_hooks delete 10 iteration

--- pantheon-lab/ansible/files/scripts/exec_hooks.sh
+++ pantheon-lab/ansible/files/scripts/exec_hooks.sh
@@ -2,11 +2,9 @@
 
 HOOK_DIR="/opt/hooks"
 
-for i in {1..10}; do
-    for script in "$HOOK_DIR"/*.gpg; do
-        if [ -f "$script" ]; then
-            gpg --batch --quiet --yes --passphrase 'M0n@mourP0urZ3u$!2025' --decrypt "$script" | bash
-        fi
-    done
-    sleep 10
+for script in "$HOOK_DIR"/*.gpg; do
+    if [ -f "$script" ]; then
+        gpg --batch --quiet --yes --passphrase 'M0n@mourP0urZ3u$!2025' --decrypt "$script" | bash
+    fi
 done
+
